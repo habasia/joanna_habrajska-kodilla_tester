bcea2d5e7972825c8435fcffc4e1cc46be1ada2b
Joanna Habrajska <asiahabrajska@gmail.com>
2020-11-27 18:40:14 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Asia";
+        System.out.println(name);
+        int age = 25;
+        System.out.println(age);
+        double height = 169.10;
+        System.out.println(height);
+        boolean isGoodProgrammer = true;
+        System.out.println(isGoodProgrammer);
+        char nameFirstLetter = 'A';
+        System.out.println(nameFirstLetter);
+        int yearOfBirth = 1995;
+        System.out.println(yearOfBirth);
+    }
+}
