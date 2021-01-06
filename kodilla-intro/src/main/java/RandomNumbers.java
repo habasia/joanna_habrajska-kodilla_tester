d9bf7b39c65a3384d2c6de3e5bf5385ef2c8f7d3
Joanna Habrajska <asiahabrajska@gmail.com>
2021-01-06 20:53:31 +0100

    06.01 commit

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,27 +1,52 @@
 import java.util.Random;
 
 public class RandomNumbers {
-    public int[] randomNumbers;
+    public int[] randomNumbersArray;
     public int size;
+    public int min;
+    public int max;
 
     //konstruktor
     public RandomNumbers() {
-        this.randomNumbers = new int[0];
+        this.randomNumbersArray = new int[0];
         this.size = 0;
+        this.min = 0;
+        this.max = 30;
     }
 
     //generator losowej liczby
     public int getRandomNumber() {
-        Random random = new Random(System.currentTimeMillis());
-        return random.nextInt(31);
+        Random random = new Random();
+        return random.nextInt(max + 1);
     }
 
     //dodawanie liczby do tablicy
-    public void addRandomNumber() {
+    public int addRandomNumber() {
         this.size++;
         int[] newRandomNumbers = new int[this.size];
-        System.arraycopy(randomNumbers,0,newRandomNumbers,0,randomNumbers.length);
+        System.arraycopy(this.randomNumbersArray, 0, newRandomNumbers, 0, randomNumbersArray.length);
         newRandomNumbers[this.size - 1] = getRandomNumber();
-        randomNumbers = newRandomNumbers;
+        this.randomNumbersArray = newRandomNumbers;
+        return this.randomNumbersArray[this.size - 1];
+    }
+
+    public int getMinimum() {
+        int min = this.max;
+        for (int i = 0; i < this.randomNumbersArray.length; i++) {
+            if (min > randomNumbersArray[i]) {
+                min = randomNumbersArray[i];
+            }
+        }
+        return min;
+    }
+
+    public int getMaximum() {
+        int max = this.min;
+        for (int i = 0; i < this.randomNumbersArray.length; i++) {
+            if (max < randomNumbersArray[i]) {
+                max = randomNumbersArray[i];
+            }
+        }
+        return max;
     }
 }
--- kodilla-intro/src/main/java/RandomNumbersMain.java
+++ kodilla-intro/src/main/java/RandomNumbersMain.java
@@ -1,11 +1,13 @@
-// nie rozumiem dlaczego nie działa, help :((
 public class RandomNumbersMain {
     public static void main(String[] args) {
         RandomNumbers random = new RandomNumbers();
         int sum = 0;
         while (sum <= 5000) {
             random.addRandomNumber();
-            sum =+ random.randomNumbers[random.size];
+            sum += random.addRandomNumber();
         }
+
+        System.out.println("Min: " + random.getMinimum());
+        System.out.println("Max: " + random.getMaximum());
     }
 }
