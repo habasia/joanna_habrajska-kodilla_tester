5435c18f78287c5aad8337a2b3b04ba3f5aec9b5
Joanna Habrajska <asiahabrajska@gmail.com>
2021-01-06 15:34:11 +0100

    06.01 commit

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,31 +1,29 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook("600g", 1000, 2017);
-        System.out.println(notebook.weight + " " + notebook.price);
-        notebook.checkPrice();
-        notebook.checkWeight();
-        notebook.checkYear();
-        notebook.checkIfWorthIt();
+        Notebook notebook = new Notebook("600g",1000,2000);
+        notebookInformation(notebook);
 
-        Notebook heavyNotebook = new Notebook("2000g", 1500, 2020);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkYear();
-        heavyNotebook.checkIfWorthIt();
 
-        Notebook oldNotebook = new Notebook("1200g", 500, 2000);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkYear();
-        oldNotebook.checkIfWorthIt();
+        Notebook heavyNotebook = new Notebook("2000g",1500,2000);
+        notebookInformation(heavyNotebook);
 
-        Notebook oldAndExpensiveNotebook = new Notebook("3000g", 3000, 1999);
-        System.out.println(oldAndExpensiveNotebook.weight + " " + oldAndExpensiveNotebook.price);
-        oldAndExpensiveNotebook.checkPrice();
-        oldAndExpensiveNotebook.checkWeight();
-        oldAndExpensiveNotebook.checkYear();
-        oldAndExpensiveNotebook.checkIfWorthIt();
+
+        Notebook oldNotebook = new Notebook("1200g",500,2000);
+        notebookInformation(oldNotebook);
     }
+
+    public static void notebookInformation (Notebook notebook)
+    {
+        System.out.println("Notebook weight is " + notebook.weight);
+        notebook.checkWeight();
+
+        System.out.println("Notebook price is " + notebook.price);
+        notebook.checkPrice();
+
+        System.out.println("Notebook year of production is " + notebook.year);
+        notebook.checkYear();
+
+        notebook.checkIfWorthIt();
+    }
+
 }
