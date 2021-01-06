82d6ebd759981a7fc54de0710c8dad9f9d6bf099
Joanna Habrajska <asiahabrajska@gmail.com>
2021-01-06 21:19:45 +0100

    06.01 commit

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -31,7 +31,7 @@ public class Application {
 
     public void checkFeatures() {
         if (name != null) {
-            if (age > 30 && height > 160) {
+            if (getAge() > 30 && getHeight() > 160) {
                 System.out.println("User is older than 30 and higher than 160cm");
             } else {
                 System.out.println("User is 30 (or younger) or 160cm (or shorter)");
