e80fa270e0db671943e341bd210048d6a67524cd
Joanna Habrajska <asiahabrajska@gmail.com>
2021-01-06 21:15:55 +0100

    06.01 commit

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,40 @@
+public class Application {
+    public String name;
+    public double age;
+    public double height;
+
+
+    public Application() {
+        this.name = "Adam";
+        this.age = 40.5;
+        this.height = 178;
+    }
+
+    public static void main(String[] args) {
+        String name = "Adam";
+        double age = 40.5;
+        double height = 178;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void checkFeatures() {
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+}
---
+++ kodilla-intro/src/main/java/ApplicationMain.java
@@ -0,0 +1,6 @@
+public class ApplicationMain {
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.checkFeatures();
+    }
+}
