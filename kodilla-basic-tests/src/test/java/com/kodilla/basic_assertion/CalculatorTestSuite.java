b968523a95c83fcc18a97369a81cd801599833c9
Joanna Habrajska <asiahabrajska@gmail.com>
2021-01-13 19:30:29 +0100

    13.01 commit

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -11,7 +11,7 @@ public class CalculatorTestSuite {
         int a = 5;
         int b = 8;
         int sumResult = calculator.sum(a, b);
-        assertEquals(14, sumResult);
+        assertEquals(13, sumResult);
     }
 
     @Test
