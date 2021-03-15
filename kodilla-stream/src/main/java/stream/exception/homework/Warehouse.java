d61b0b5464c4eaac896add03d7cd588da9538f54
Joanna Habrajska <asiahabrajska@gmail.com>
2021-03-15 21:45:56 +0100

    03.02 commit

--- kodilla-stream/src/main/java/stream/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/stream/exception/homework/Warehouse.java
@@ -1,14 +1,7 @@
 package stream.exception.homework;
 
-import stream.User;
-import stream.UsersManager;
-import stream.UsersRepository;
-import stream.exception.AirportNotFoundException;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Warehouse {
     private List<Order> orders;
--- kodilla-stream/src/main/java/stream/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/stream/exception/homework/WarehouseApp.java
@@ -1,8 +1,6 @@
 package stream.exception.homework;
 
-import stream.exception.AirportNotFoundException;
-
-import java.util.List;
+//hejka moglbys zajrzec mi w kodzik? caly czas dostaje blad, obsluguje go pozniej, wiec program sie nie wywala, ale chyba nie udalo mi sie dodac orderow do tablicy w obiekcie warehouse
 
 public class WarehouseApp {
     public static void main(String[] args) {
@@ -10,12 +8,13 @@ public class WarehouseApp {
         warehouse.addOrder("111");
         warehouse.addOrder("123");
         warehouse.addOrder("222");
+        //System.out.println(warehouse.getOrders().toString());
 
         try {
             Order getNonExistingOrder = warehouse.getOrder("111");
-            System.out.println("Order found");
+            System.out.println("Order found" + getNonExistingOrder.getNumber() + ".");
         } catch (OrderDoesntExistException e) {
-            System.out.println("Order hasn't been found");
+            System.out.println("Order hasn't been found.");
         } finally {
             System.out.println("Thank you, goodbye!");
         }
