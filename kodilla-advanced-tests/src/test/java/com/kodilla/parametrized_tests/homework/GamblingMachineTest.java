f4ccdee1a84b4fdb4865d72a8432c7790c8eafdb
Joanna Habrajska <asiahabrajska@gmail.com>
2021-03-21 17:38:27 +0100

    21.03 commit

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -3,6 +3,7 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,30 +12,33 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTest {
-    GamblingMachine gamblingMachine = new GamblingMachine();
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineAcceptableValues.csv")
+    public void succeedWhenTheValueIsBetweenOneAndFourtyNineAndContainsSixDifferentNumbers(String input) throws InvalidNumbersException {
+        String[] numbersAsStrings = input.split(" ");
+        Set<Integer> numbers = new HashSet<>();
+        for (String number :
+                numbersAsStrings) {
+            numbers.add(Integer.parseInt(number));
+        }
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        int result = gamblingMachine.howManyWins(numbers);
+        assertTrue(result >= 0 && result <= 6);
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/gamblingMachineUnacceptableValues.csv")
-    public void shouldAcceptOnlyValidSetOfNumbers(int one, int two, int three, int four, int five, int six) throws InvalidNumbersException {
-        Set<Integer> integers = new HashSet<>();
-        integers.add(one);
-        integers.add(two);
-        integers.add(three);
-        integers.add(four);
-        integers.add(five);
-        integers.add(six);
-
-
+    public void failWhenTheValueIsNotBetweenOneAndFourtyNineAndContainsSixDifferentNumbers(String input) throws InvalidNumbersException {
+        Set<Integer> numbers = Arrays.stream(input.split(" ")).map(Integer::parseInt).collect(Collectors.toSet());
         GamblingMachine gamblingMachine = new GamblingMachine();
-
-        assertThrows(InvalidNumbersException.class, ()->gamblingMachine.howManyWins(integers));
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(numbers));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/gamblingMachineUnacceptableLengthOfSet.csv")
-    public void shouldThrowException(String input) {
-        Set<Integer> inputSet = Stream.of(input.split(",")).map(Integer::valueOf).collect(Collectors.toCollection(HashSet::new));
-
-        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(inputSet));
+    public void failWhenThereAreNotEnoughNumbers(String input) throws InvalidNumbersException {
+        Set<Integer> numbers = Arrays.stream(input.split(" ")).map(Integer::parseInt).collect(Collectors.toSet());
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(numbers));
     }
 }
