e1ae6d13432a61e0be6ffa353476bb74075a5247
Joanna Habrajska <asiahabrajska@gmail.com>
2021-03-21 16:33:06 +0100

    21.03 commit

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -1,10 +1,7 @@
 package com.kodilla.parametrized_tests;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -0,0 +1,40 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamblingMachineTest {
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineUnacceptableValues.csv")
+    public void shouldGenerateComputerNumbers(int one, int two, int three, int four, int five, int six) throws InvalidNumbersException {
+        Set<Integer> integers = new HashSet<>();
+        integers.add(one);
+        integers.add(two);
+        integers.add(three);
+        integers.add(four);
+        integers.add(five);
+        integers.add(six);
+
+
+        GamblingMachine gamblingMachine = new GamblingMachine();
+
+        assertThrows(InvalidNumbersException.class, ()->gamblingMachine.howManyWins(integers));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineUnacceptableLengthOfSet.csv")
+    public void shouldThrowException(String input) {
+        Set<Integer> inputSet = Stream.of(input.split("-")).map(Integer::valueOf).collect(Collectors.toCollection(HashSet::new));
+
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(inputSet), "Wrong numbers provided");
+    }
+}
