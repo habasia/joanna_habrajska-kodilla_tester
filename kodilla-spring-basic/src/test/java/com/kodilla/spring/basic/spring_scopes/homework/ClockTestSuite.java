9c1f59cdde30dae817d8e5dc90d55387b767a641
Joanna Habrajska <asiahabrajska@gmail.com>
2021-03-29 11:01:03 +0200

    29.03 commit

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ClockTestSuite {
+
+    @Test
+    public void shouldCreateDifferentTasks() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_scopes.homework");
+        Clock firstBean = context.getBean(Clock.class);
+        Clock secondBean = context.getBean(Clock.class);
+        Clock thirdBean = context.getBean(Clock.class);
+        Assertions.assertNotEquals(firstBean, secondBean);
+        Assertions.assertNotEquals(secondBean, thirdBean);
+        Assertions.assertNotEquals(firstBean, thirdBean);
+    }
+}
